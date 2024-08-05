f1192632df005815063ccab82ee09d8cd0e33b49
Bimo Dwien Prabowo <70501000+bimodwien@users.noreply.github.com>
2024-08-05 12:46:22 +0700

    Fin 82 (#17)
    * feat: backend week 3 done
    
    * feat: stocks done
    
    * feat: stock update done
    
    * feat: product discount BE
    
    * feat: discount voucher create done
    
    * feat: dashboard reports
    
    * feat: debug admin auth
    
    * feat: adding sweet alert to all dashboard
    
    * feat: voucher not using product id
    
    * feat: report FE
    
    * feat: deleting console log from middleware ts

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,13 +4,9 @@ import { jwtDecode } from 'jwt-decode';
 import { TUser } from './models/user';
 
 export async function middleware(request: NextRequest) {
-  // const refresh_token = request.cookies.get('access_token')?.value || '';
   const refresh_token = request.cookies.get('refresh_token')?.value || '';
   const response = NextResponse.next();
   const { pathname } = request.nextUrl;
-
-  console.log('refresh_token: ', refresh_token);
-
   const isLogin = await fetch('http://localhost:8000/v1/v3', {
     method: 'GET',
     headers: {
@@ -28,72 +24,36 @@ export async function middleware(request: NextRequest) {
       if (err instanceof Error) console.log(err.message);
       return false;
     });
-
-  console.log('isLogin: ', isLogin);
-
   const token = response.cookies.get('access_token')?.value;
-
-  console.log(`Access Token: ${token}`);
-
   const decode = token ? (jwtDecode(token) as { user: TUser }) : undefined;
-
-  console.log(`Decoded Token: ${JSON.stringify(decode)}`);
-
   const isCustomer = decode?.user?.role === 'user' ? true : false;
   const isSuperAdmin = decode?.user?.role === 'superAdmin';
   const isStoreAdmin = decode?.user?.role === 'storeAdmin';
-
-  console.log(
-    `Roles - isCustomer: ${isCustomer}, isSuperAdmin: ${isSuperAdmin}, isStoreAdmin: ${isStoreAdmin}`,
-  );
-
-  // if (!isSuperAdmin && pathname.startsWith('/dashboard')) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // } else if (
-  //   (pathname == '/login' || pathname == '/register') &&
-  //   isLogin &&
-  //   isCustomer
-  // )
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // else if ((pathname == '/' || pathname.startsWith('/dashboard')) && !isLogin)
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // else if (pathname == '/' && isLogin && !isCustomer)
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // else if (pathname == '/' && isLogin && isStoreAdmin)
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // else if (pathname == '/dashboard' && isLogin && isStoreAdmin) {
-  //   return response;
-  // }
-  // return response;
   if (!isLogin) {
-    console.log('User is not logged in');
     if (pathname !== '/login' && pathname !== '/signUp') {
-      console.log('Redirecting to /login');
       return NextResponse.redirect(new URL('/login', request.url));
     }
   } else {
     if (isSuperAdmin) {
-      console.log('SuperAdmin is logged in');
-      if (pathname === '/login' || pathname === '/signUp') {
-        console.log('Redirecting SuperAdmin to /dashboard');
+      if (pathname === '/login' || pathname === '/signUp' || pathname === '/') {
         return NextResponse.redirect(new URL('/dashboard', request.url));
+      }
+      if (pathname === '/dashboard') {
+        return response;
       }
     } else if (isStoreAdmin) {
-      console.log('StoreAdmin is logged in');
       if (pathname === '/' || pathname === '/login' || pathname === '/signUp') {
-        console.log('Redirecting StoreAdmin to /dashboard');
         return NextResponse.redirect(new URL('/dashboard', request.url));
       }
+      if (pathname === '/dashboard') {
+        return response;
+      }
     } else if (isCustomer) {
-      console.log('Customer is logged in');
       if (pathname === '/login' || pathname === '/signUp') {
-        console.log('Redirecting Customer to /');
         return NextResponse.redirect(new URL('/', request.url));
       }
     }
   }
-
-  console.log('Default response');
   return response;
 }
 export const config = {
